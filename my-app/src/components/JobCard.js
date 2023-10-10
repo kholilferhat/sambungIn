4a69b83143e7e1154ec9934cfab213495ef41f76
Kholil Ferhat <kholilferhat@gmail.com>
2023-10-10 23:26:50 +0700

    feat: layout

---
+++ my-app/src/components/JobCard.js
@@ -0,0 +1,11 @@
+import { Button, Text, View } from "react-native";
+import { styles } from "../utilities/StyleSheet";
+
+export default JobCard = () => {
+    return (
+        <View>
+            <Text>ini card</Text>
+        </View>
+
+    )
+}
--- my-app/src/views/Homepage.js
+++ my-app/src/views/Homepage.js
@@ -1,17 +1,45 @@
-import { Button, Text, View } from "react-native";
+import { Button, ScrollView, Text, View } from "react-native";
 import { styles } from "../utilities/StyleSheet";
+import JobCard from "../components/JobCard";
 
 
 
-export default function HomePage({navigation}) {
-    return (
-      <View style={styles.container}>
-        <Text>ini home</Text>
-        <Button 
-        onPress={() => {navigation.navigate('Detail')}}
-        title="Detail"
-      
-        />
-      </View>
-    );
-  }
+export default function HomePage({ navigation }) {
+  return (
+    // <View style={styles.container}>
+    // <View style={styles.heroContainer}>
+    //   <Text>ini home</Text>
+    //   <Button
+    //     onPress={() => { navigation.navigate('Detail') }}
+    //     title="Detail"
+    //   />
+    // </View>
+    <ScrollView
+      style={styles.scrollContainer}
+    // contentContainerStyle={styles.cardContainer}
+    >
+      <ScrollView
+        style={styles.heroContainer}
+        contentContainerStyle={styles.bannerContainer}
+        nestedScrollEnabled={true}
+      >
+        <View style={styles.bannerCard}>
+          <Text>ini home</Text>
+          <Button
+            onPress={() => { navigation.navigate('Detail') }}
+            title="Detail"
+          />
+        </View>
+        <View style={styles.bannerCard}>
+          <Text>ini home</Text>
+          <Button
+            onPress={() => { navigation.navigate('Detail') }}
+            title="Detail"
+          />
+        </View>
+      </ScrollView>
+      <JobCard style={styles.cardContainer} />
+    </ScrollView>
+    // </View>
+  );
+}
